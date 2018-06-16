9b3ed8387b7e6317469921f9307a4944d953712d
orris27 <40049332+orris27@users.noreply.github.com>
2018-06-16 19:21:12 +0800

    Add files via upload

---
+++ thread/global_alter.py
@@ -0,0 +1,40 @@
+'''
+    Modifying the same global variable at the same time using threads will cause problem
+'''
+import threading
+import time
+
+money=0
+
+def typing():
+    global money
+    for i in range(100000):
+        money+=1
+    print('money in typing is %d'%(money))
+
+def watering():
+    global money
+    for i in range(300000):
+        money+=1
+    print('money in watering is %d'%(money))
+
+def check():
+    global money
+    print('money in check is %d'%(money))
+
+
+t1 = threading.Thread(target=typing)
+t1.start()
+
+#time.sleep(1)
+
+t2 = threading.Thread(target=watering)
+t2.start()
+
+#time.sleep(1)
+
+
+t3 = threading.Thread(target=check)
+t3.start()
+
+
