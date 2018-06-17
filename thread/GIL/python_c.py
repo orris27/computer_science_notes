759dc6bc01371a63d52347aa63e6e599a683a011
orris27 <40049332+orris27@users.noreply.github.com>
2018-06-17 10:55:57 +0800

    Update python_c.py

--- thread/GIL/python_c.py
+++ thread/GIL/python_c.py
@@ -5,9 +5,9 @@ def deadloop():
     while True:
         pass
 
-lib=ctypes.cdll.LoadLibrary("./deadloop.so")
+lib=ctypes.cdll.LoadLibrary("./deadloop.so") # open the library
 
-t=threading.Thread(target=lib.deadloop)
+t=threading.Thread(target=lib.deadloop) # use the "deadloop" function in the lib
 t.start()
 
 
