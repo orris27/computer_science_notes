83ca4c9ce18d9f9903fb819b105f7d0cd9db7bf6
orris27 <40049332+orris27@users.noreply.github.com>
2018-06-17 16:48:02 +0800

    Add files via upload

--- network/udp/udp_response.py
+++ network/udp/udp_response.py
@@ -1,10 +1,24 @@
 import socket
+import sys
 
-curr_ip='10.189.177.245'
+if len(sys.argv) != 3:
+    print('-'*30)
+    print('tips:')
+    print('python xxx.py 10.189.190.100 2334')
+    print('-'*30)
+    exit()
+
+else:
+    curr_ip=sys.argv[1]
+    curr_port=int(sys.argv[2])
+
+
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-s.bind((curr_ip,2334))
+
+s.bind((curr_ip,curr_port))
+#print(s.getsockname())
 
 data=s.recvfrom(1024) # data is a 2d tuple. (raw_data,src_ip)
 
