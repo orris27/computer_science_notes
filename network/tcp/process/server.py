b18355a51cc2841c04ef441421548b4268683203
orris27 <40049332+orris27@users.noreply.github.com>
2018-06-18 16:43:42 +0800

    Add files via upload

---
+++ network/tcp/process/server.py
@@ -0,0 +1,49 @@
+'''
+    Create a server that 1)can only serve at most one person at the same time 2)print the message from the client 3)send back a "thank you" message
+    !!Important: Main process should close client_socket
+    1. create a socket
+    2. enable the socket to bind a same port
+    3. bind
+    4. listen
+    5. begin to accept forever
+    6.      if ok, then receive&print message from the client until the client closed socket
+    7.      close the client_socket
+    8.      back to the step#5
+'''
+import socket
+import multiprocessing
+
+def handle_client(client_socket,client_addr):
+    try:
+        while True:
+        
+            received_data=client_socket.recv(1024)
+            
+            if len(received_data) >0:
+                print("%s:%s"%(str(client_addr),received_data.decode('utf-8')))
+                client_socket.send('Thank you!'.encode('utf-8'))
+
+            else:
+                print("service for %s stops"%(str(client_addr)))
+                break
+    finally:
+        client_socket.close()
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+    my_addr=('127.0.0.1',2333)
+    s.bind(my_addr)
+
+    s.listen(5)
+
+
+    while True:
+
+        client_socket,client_addr=s.accept()
+        
+        t = multiprocessing.Process(target=handle_client,args=(client_socket,client_addr))
+        t.start()
+
+        client_socket.close() # !!important 
