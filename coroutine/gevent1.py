32ca6d022b6ccc839ae5a65c1a7fd8099a06ae44
orris27 <40049332+orris27@users.noreply.github.com>
2018-06-18 23:54:33 +0800

    Update gevent1.py

--- coroutine/gevent1.py
+++ coroutine/gevent1.py
@@ -12,9 +12,9 @@ def f(n):
         # gevent requires its own function. (X time.sleep(1))
         gevent.sleep(1)
 
-g1=gevent.spawn(f,5)
+g1=gevent.spawn(f,5) # function name,args
 g2=gevent.spawn(f,5)
 g3=gevent.spawn(f,5)
-g1.join()
+g1.join() # 如果我注释了g2.join()和g3.join()的话,只保留g1.join()也会切换到所有spawn都执行完,这样程序才结束
 g2.join()
 g3.join()
