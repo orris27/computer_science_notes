055f2600be43134c23503ba5f57dbd90a719d3f2
orris27 <40049332+orris27@users.noreply.github.com>
2018-08-20 04:11:40 +0800

    Update clear.sh

--- bigdata/hadoop/scripts/clear.sh
+++ bigdata/hadoop/scripts/clear.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
 NODES="hadoop01 hadoop02 hadoop03"
-HDFS_DIR=/data
+HDFS_DIR=/hdfs
+HADOOP_INSTALL=/usr/local/hadoop
 
 for i in ${NODES}
 do
     ssh "$i" rm -rf ${HDFS_DIR}/*
-    ssh "$i" /usr/local/hadoop/bin/hdfs namenode -format 
+    ssh "$i" ${HADOOP_INSTALL}/bin/hdfs namenode -format 
 done
