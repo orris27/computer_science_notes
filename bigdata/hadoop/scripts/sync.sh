97584d9330ed80ebfa2353c85b997fbefcc35a6b
orris27 <40049332+orris27@users.noreply.github.com>
2018-08-20 04:12:22 +0800

    Update sync.sh

--- bigdata/hadoop/scripts/sync.sh
+++ bigdata/hadoop/scripts/sync.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 WORKER_NODES="hadoop02 hadoop03"
-HDFS_DIR=/data
 HADOOP_INSTALL=/usr/local/hadoop
 filename=$1
 
@@ -9,3 +8,4 @@ for i in ${WORKER_NODES}
 do
     scp ${HADOOP_INSTALL}/etc/hadoop/"$filename" root@"$i":${HADOOP_INSTALL}/etc/hadoop/"$filename"
 done
+HDFS_DIR=/data
