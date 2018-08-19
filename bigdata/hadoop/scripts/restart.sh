8ebd7db11c1dcd44e3dd8a42d6d9a2dc535e5391
orris27 <40049332+orris27@users.noreply.github.com>
2018-08-20 03:59:47 +0800

    Add files via upload

---
+++ bigdata/hadoop/scripts/restart.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+NODES="hadoop01 hadoop02 hadoop03"
+HDFS_DIR=/data
+HADOOP_INSTALL=/usr/local/hadoop
+
+
+# stop hadoop
+${HADOOP_INSTALL}/sbin/stop-yarn.sh
+${HADOOP_INSTALL}/sbin/stop-dfs.sh
+
+
+# clean 
+for i in ${NODES}
+do
+    ssh "$i" rm -rf ${HDFS_DIR}/*
+    ssh "$i" /usr/local/hadoop/bin/hdfs namenode -format 
+done
+
+
+# start hadoop
+${HADOOP_INSTALL}/sbin/start-yarn.sh
+${HADOOP_INSTALL}/sbin/start-dfs.sh
